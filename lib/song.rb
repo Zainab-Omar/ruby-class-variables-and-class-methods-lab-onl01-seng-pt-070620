87b4837bf9a6595af170b5a6fa4b044ff1d5d349
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-24 22:51:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,8 +25,8 @@ class Song
   def self.genre_count
     genre_hash={}
     @@genres.each do |genre|
-      genre_hash[genre] ||= []
-      genre_hash +=1
+      genre_hash[genre] +=1
+    end
   end
     
 end
